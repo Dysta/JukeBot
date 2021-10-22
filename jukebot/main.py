90e6380d65baae22b6f2e5c25e94dcd7e23cd185
Dysta <xav4098@gmail.com>
2021-10-22 16:57:10 +0200

    [Enh] Add logging

--- jukebot/main.py
+++ jukebot/main.py
@@ -1,3 +1,5 @@
+import datetime
+import logging
 from discord.ext import commands
 from dotenv import load_dotenv
 from jukebot import JukeBot
@@ -7,6 +9,19 @@ from utility import Utiliy
 
 
 def main():
+    # setting up logging
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(
+        filename=f"./logs/{datetime.datetime.now():%Y-%m-%d}-jukebot.log",
+        encoding="utf-8",
+        mode="w",
+    )
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    )
+    logger.addHandler(handler)
+
     load_dotenv()
     # get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value.
     # to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link
