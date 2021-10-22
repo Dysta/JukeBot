0dd5b4bd9831ea3f9a7df3ade8b0d78da21dd6e8
Dysta <xav4098@gmail.com>
2021-10-22 16:55:31 +0200

    [Enh] Edit .env variable name
    Also change the invocation of the bot by the prefix set in .env file or a mention

--- jukebot/main.py
+++ jukebot/main.py
@@ -1,3 +1,4 @@
+from discord.ext import commands
 from dotenv import load_dotenv
 from jukebot import JukeBot
 import os
@@ -7,12 +8,13 @@ from utility import Utiliy
 
 def main():
     load_dotenv()
-    # client.run(os.getenv("BETA_TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value.
-    # to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
-    # enjoy!
-    bot = JukeBot(command_prefix="!", help_message=None)
+    # get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value.
+    # to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link
+    # to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging
+    # the flask server enjoy!
+    bot = JukeBot(command_prefix=commands.when_mentioned_or(os.environ["BOT_PREFIX"]))
     bot.add_cog(Utiliy(bot))
-    bot.run(os.environ["TOKEN"])
+    bot.run(os.environ["BOT_TOKEN"])
 
 
 if __name__ == "__main__":
