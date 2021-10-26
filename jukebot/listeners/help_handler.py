fed51e44635ce7f5f51176b68742d9eca66105a2
Dysta <xav4098@gmail.com>
2021-10-26 19:43:04 +0200

    [Enh] Implement the HelpCommand class
    Used by the lib to generate the help message of the bot

---
+++ jukebot/listeners/help_handler.py
@@ -0,0 +1,80 @@
+from discord.ext import commands
+
+import itertools
+from embed import Embed
+
+
+class HelpHandler(commands.HelpCommand):
+    def __init__(self):
+        opts = {
+            "aliases": ["h"],
+            "brief": "Show the help message",
+            "help": "Display a list of all available commands.\nCan display help for a specific category or command.",
+            "usage": "help [command_name|category_name]",
+        }
+        super().__init__(command_attrs=opts)
+
+    def get_ending_note(self):
+        command_name = self.invoked_with
+        return (
+            f"Type `{self.context.prefix}{command_name} command` for more info on a command.",
+            f"Type `{self.context.prefix}{command_name} category` for more info on a category.",
+        )
+
+    async def send_bot_help(self, mapping):
+        ctx = self.context
+        bot = ctx.bot
+
+        help_embed = Embed.info_message(ctx, title="Available commands")
+
+        def get_category(command, *, no_category="Other:"):
+            cog = command.cog
+            return cog.qualified_name if cog is not None else no_category
+
+        filtered = await self.filter_commands(bot.commands, sort=True, key=get_category)
+        to_iterate = itertools.groupby(filtered, key=get_category)
+
+        for category, cmds in to_iterate:
+            cmds = sorted(cmds, key=lambda c: c.name)
+            cmds_str = ", ".join([f"{c.name}" for c in cmds])
+            help_embed.add_field(name=category, value=f"```{cmds_str}```", inline=False)
+
+        note = self.get_ending_note()
+        if note:
+            help_embed.add_field(name="Note", value="\n".join(note), inline=False)
+
+        await ctx.send(embed=help_embed)
+
+    async def send_cog_help(self, cog):
+        ctx = self.context
+
+        cog_embed = Embed.info_message(ctx, title="Available commands")
+
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        cmds = sorted(filtered, key=lambda c: c.name)
+        cmds_str = "\n".join([f"{c.name} - {c.short_doc}" for c in cmds])
+
+        cog_embed.add_field(
+            name=cog.qualified_name, value=f"```{cmds_str}```", inline=False
+        )
+        note = self.get_ending_note()
+        if note:
+            cog_embed.add_field(name="Note", value=note[0], inline=False)
+
+        await ctx.send(embed=cog_embed)
+
+    async def send_command_help(self, command):
+        ctx = self.context
+
+        cmd_embed = Embed.info_message(
+            ctx, title=f"Command : {command.name}", content=command.help
+        )
+
+        aliases = ", ".join([a for a in [command.name] + command.aliases])
+        cmd_embed.add_field(name="Aliases", value=f"```{aliases}```", inline=False)
+
+        cmd_embed.add_field(
+            name="Usage", value=f"```{ctx.prefix}{command.usage}```", inline=False
+        )
+
+        await ctx.send(embed=cmd_embed)
