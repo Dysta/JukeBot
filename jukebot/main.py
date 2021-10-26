bcd3d20bafeccba7f791ecee5462da6986a3075a
Dysta <xav4098@gmail.com>
2021-10-26 19:40:31 +0200

    [Enh] Add HelpHandler to the bot

--- jukebot/main.py
+++ jukebot/main.py
@@ -8,7 +8,7 @@ from jukebot import JukeBot
 import os
 
 from cogs import Utility
-from listeners import ErrorHandler
+from listeners import ErrorHandler, HelpHandler
 
 
 def main():
@@ -30,9 +30,13 @@ def main():
     # to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link
     # to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging
     # the flask server enjoy!
-    bot = JukeBot(command_prefix=commands.when_mentioned_or(os.environ["BOT_PREFIX"]))
+    bot = JukeBot(
+        command_prefix=commands.when_mentioned_or(os.environ["BOT_PREFIX"]),
+        help_command=HelpHandler(),
+    )
     bot.add_cog(ErrorHandler(bot))
     bot.add_cog(Utility(bot))
+    bot.help_command.cog = bot.get_cog("Utility")
     bot.run(os.environ["BOT_TOKEN"])
 
 
