00780a550cc0ac2062d5339ffbe380429782b217
Dysta <xav4098@gmail.com>
2021-11-08 20:37:34 +0100

    [Enh] Add method to convert str time to youtube format

--- jukebot/utils.py
+++ jukebot/utils.py
@@ -1,5 +1,12 @@
+import re
+
+
 def seconds_converter(seconds: int) -> (int, int, int, int):
     h, r = divmod(seconds, 3600)
     m, s = divmod(r, 60)
     d, h = divmod(h, 24)
     return d, h, m, s
+
+
+def convert_time_to_youtube_format(time: (int, int, int, int)) -> str:
+    return re.sub(r"^[0:]*(.+:..)$", r"\1", ":".join([f"{e:02d}" for e in time]))
