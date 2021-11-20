ec1cf26b87b75be03441d32f4457cfda53ba3e2f
Dysta <xav4098@gmail.com>
2021-11-20 18:22:56 +0100

    [Enh] Switch resultset and songset to abstractset
    TBS miss typo

--- jukebot/components/resultset.py
+++ jukebot/components/resultset.py
@@ -1,33 +1,16 @@
-from collections import abc
 from dataclasses import dataclass
-from typing import Iterator, List
+from typing import List
 
+from jukebot.abstract_components import AbstractSet
 from jukebot.components import Result, Query
 
 
 @dataclass
-class ResultSet(abc.Collection):
-    results: List[Result]
-
-    def __len__(self) -> int:
-        return len(self.results)
-
-    def __iter__(self) -> Iterator[Result]:
-        for r in self.results:
-            yield r
-
-    def __contains__(self, r: object) -> bool:
-        return r in self.results
-
-    def __getitem__(self, idx: int) -> Result:
-        if 0 <= idx < len(self):
-            return self.results[idx]
-        raise IndexError
-
+class ResultSet(AbstractSet[Result]):
     @classmethod
     def from_query(cls, query: Query) -> "ResultSet":
         results: List[Result] = []
         for r in query.entries:
             results.append(Result.from_entry(r))
 
-        return cls(results=results)
+        return cls(set=results)
--- jukebot/components/songset.py
+++ jukebot/components/songset.py
@@ -1,33 +1,16 @@
-from collections import abc
 from dataclasses import dataclass
-from typing import Iterator, List
+from typing import List
 
+from jukebot.abstract_components import AbstractSet
 from jukebot.components import Song, Query
 
 
 @dataclass
-class SongSet(abc.Collection):
-    songs: List[Song]
-
-    def __len__(self) -> int:
-        return len(self.songs)
-
-    def __iter__(self) -> Iterator[Song]:
-        for s in self.songs:
-            yield s
-
-    def __contains__(self, s: object) -> bool:
-        return s in self.songs
-
-    def __getitem__(self, idx: int) -> Song:
-        if 0 <= idx < len(self):
-            return self.songs[idx]
-        raise IndexError
-
+class SongSet(AbstractSet[Song]):
     @classmethod
     def from_query(cls, query: Query) -> "SongSet":
         songs: List[Song] = []
         for e in query.entries:
             songs.append(Song.from_entry(e))
 
-        return cls(songs=songs)
+        return cls(set=songs)
