fc37c180cbe5f0fa45355c650cf1a7ecc60a8bc6
Dysta <xav4098@gmail.com>
2021-12-02 12:54:19 +0100

    [Enh] Add method to sanitize result from yt-dlp

--- jukebot/components/query.py
+++ jukebot/components/query.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from typing import Optional, Union
 
 import yt_dlp
 
@@ -9,7 +11,7 @@ yt_dlp.utils.bug_reports_message = lambda: ""
 class Query:
     def __init__(self, query: str):
         self._query: str = query
-        self._info: dict = {}
+        self._result: Optional[Union[dict, list]] = None
         self._success: bool = False
 
     async def search(self):
@@ -30,34 +32,29 @@ class Query:
             info = await loop.run_in_executor(
                 None, lambda: ytdl.extract_info(url=self._query, **ytdl_kwargs)
             )
+            info = Query._sanitize_info(info)
 
         # with open("debug.json", "w") as f:
         #     json.dump(info, f)
 
-        self._info = info
+        self._result = info
         self._success = True
 
-    def get_entries(self, number: int):
-        if "entries" in self._info:
-            try:
-                return self._info["entries"][number]
-            except KeyError:
-                raise
-        return self._info
+    @staticmethod
+    def _sanitize_info(info: dict) -> Union[dict, list]:
+        if "entries" in info:
+            info = list(info["entries"])
+            if len(info) == 1:
+                info = info[0]
+        return info
 
     @property
     def success(self):
         return self._success
 
     @property
-    def info(self):
-        return self._info
-
-    @property
-    def entries(self):
-        if "entries" in self._info:
-            return self._info["entries"]
-        return [self._info]
+    def results(self) -> list:
+        return self._result
 
 
 class _RequestOption:
