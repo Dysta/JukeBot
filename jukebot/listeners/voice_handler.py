5dc4040ac056f09a7c04f5c3cb6144fca7ca5c75
Dysta <xav4098@gmail.com>
2021-12-12 21:58:40 +0100

    [Enh] Add event when bot is alone and when someone connect

--- jukebot/listeners/voice_handler.py
+++ jukebot/listeners/voice_handler.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from nextcord import Member, VoiceState, VoiceChannel
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot
@@ -14,13 +16,37 @@ class VoiceHandler(commands.Cog):
     async def on_voice_state_update(
         self, member: Member, before: VoiceState, after: VoiceState
     ):
-        # TODO: FINIR
-        channel: VoiceChannel = before.channel or after.channel
-        print(f"{channel.members=}")
-        print(f"{len(channel.members)=}")
-        # if len(channel.members) == 1 and sum(1 for m in channel.members if m.bot):
-        #     player: Player = self._players[channel.guild.id]
-        #     player.pause()
+        if before.channel != after.channel:
+            if before.channel is None:
+                self.bot.dispatch("voice_channel_connect", member, after.channel)
+            elif after.channel is None:
+                self.bot.dispatch("voice_channel_disconnect", member, before.channel)
+                if len(before.channel.members) == 1:
+                    self.bot.dispatch("voice_channel_alone", member, before.channel)
+            else:
+                self.bot.dispatch(
+                    "voice_channel_move", member, before.channel, after.channel
+                )
+            # if after.afk:
+            #     self.bot.dispatch("voice_afk", member, before.channel)
+
+    @commands.Cog.listener()
+    async def on_voice_channel_connect(self, member: Member, channel: VoiceChannel):
+        if member == self.bot.user:
+            return
+        if self.bot.user in channel.members:
+            player: Player = self._players[channel.guild.id]
+            if player.stream and not player.playing:
+                ctx = player.context
+                await self.bot.get_cog("Music").resume(context=ctx)
+
+    @commands.Cog.listener()
+    async def on_voice_channel_alone(self, member: Member, channel: VoiceChannel):
+        if channel.members[0].id == self.bot.user.id:
+            player: Player = self._players[channel.guild.id]
+            if player.playing:
+                ctx = player.context
+                await self.bot.get_cog("Music").pause(context=ctx)
 
 
 def setup(bot):
