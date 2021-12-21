aa578d62540467369f7c9f07571135f8267a8028
Dysta <xav4098@gmail.com>
2021-12-21 12:11:52 +0100

    [Enh] Refactor audio stream class

--- jukebot/components/audio_stream.py
+++ jukebot/components/audio_stream.py
@@ -1,21 +1,44 @@
-from nextcord import FFmpegPCMAudio, AudioSource
+import platform
+import sys
+from io import SEEK_CUR
+
+from nextcord import FFmpegPCMAudio
+from nextcord.opus import Encoder as OpusEncoder
 
 
-class AudioStream(AudioSource):
-    def __init__(self, source: FFmpegPCMAudio):
-        self._source: FFmpegPCMAudio = source
+class AudioStream(FFmpegPCMAudio):
+    def __init__(self, stream_url: str):
+        super(AudioStream, self).__init__(
+            stream_url,
+            executable=_PlayerOption.FFMPEG_EXECUTABLE[platform.system()],
+            pipe=False,
+            stderr=sys.stdout,  # None,  # subprocess.PIPE
+            before_options=_PlayerOption.FFMPEG_BEFORE_OPTIONS,  # "-nostdin",
+            options=_PlayerOption.FFMPEG_OPTIONS,
+        )
         self._progress: int = 0
 
     def read(self) -> bytes:
-        data = self._source.read()
-        if data:
-            self._progress += 1
-        return data
+        ret = self._stdout.read(OpusEncoder.FRAME_SIZE)
+        if len(ret) != OpusEncoder.FRAME_SIZE:
+            return b""
+        self._progress += 1
+        return ret
 
-    def cleanup(self) -> None:
-        self._source.cleanup()
+    def seek(self, sec: int = 1):
+        self._stdout.seek(sec, SEEK_CUR)
 
     @property
     def progress(self) -> int:
         # 20ms
         return int(self._progress * 0.02)
+
+
+class _PlayerOption:
+    FFMPEG_EXECUTABLE = {"Linux": "./bin/ffmpeg", "Windows": "./bin/ffmpeg.exe"}
+
+    FFMPEG_BEFORE_OPTIONS = " ".join(
+        ["-reconnect 1", "-reconnect_streamed 1", "-reconnect_delay_max 3", "-nostdin"]
+    )
+
+    FFMPEG_OPTIONS = "-vn"
