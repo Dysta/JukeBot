2dcf8f084ae4aec4a82f5e3322b0c6f015cfeb78
Dysta <xav4098@gmail.com>
2022-01-03 18:56:50 +0100

    [Enh] Add chech to verify if queue is empty

--- jukebot/checks/user.py
+++ jukebot/checks/user.py
@@ -1,7 +1,16 @@
 from nextcord.ext.commands import Context, CheckFailure
 
+from jukebot.components import Player
+
 
 def is_dysta(ctx: Context):
     if not ctx.message.author.id == 845189167914549288:
         raise CheckFailure("You're not Dysta.")
+    return True
+
+
+def bot_queue_is_not_empty(ctx: Context):
+    player: Player = ctx.bot.players[ctx.guild.id]
+    if not len(player.queue):
+        raise CheckFailure("The queue is empty.")
     return True
