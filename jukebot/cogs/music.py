5b56ff0dd8fbfde471654b47ab798c87a6920c18
Dysta <xav4098@gmail.com>
2022-01-05 18:52:00 +0100

    [Enh] Display or not when a music is added

--- jukebot/cogs/music.py
+++ jukebot/cogs/music.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from nextcord import Embed, Member
+from nextcord import Embed
 from nextcord.ext import commands
 from nextcord.ext.commands import Context, BucketType, Bot
 
@@ -178,7 +178,7 @@ class Music(commands.Cog):
     @commands.check(voice.bot_and_user_in_same_channel)
     @commands.check(voice.bot_is_connected)
     @commands.check(voice.user_is_connected)
-    async def add(self, ctx: Context, *, query: str):
+    async def add(self, ctx: Context, silent: Optional[bool] = False, *, query: str):
         query = query if regex.is_url(query) else f"ytsearch1:{query}"
         with ctx.typing():
             qry: Query = Query(query)
@@ -199,15 +199,15 @@ class Music(commands.Cog):
             e: Embed = embed.queue_message(
                 ctx.author, res, title=f"Enqueued : {len(res)} songs"
             )
+            await ctx.send(embed=e)
         else:
             res: Result = Result.from_query(qry)
             res.requester = ctx.author
             player: Player = self.bot.players[ctx.guild.id]
             player.queue.put(res)
-
-            e: Embed = embed.result_enqueued(ctx.author, res)
-
-        await ctx.send(embed=e)
+            if not silent:
+                e: Embed = embed.result_enqueued(ctx.author, res)
+                await ctx.send(embed=e)
 
     @commands.command(
         aliases=["r"],
