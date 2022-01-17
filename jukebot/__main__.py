e5b5ad14612d670692d40cc2b791c9ddc222b291
Dysta <xav4098@gmail.com>
2022-01-17 14:55:06 +0100

    [Enh] Handle nextcord logs

--- jukebot/__main__.py
+++ jukebot/__main__.py
@@ -70,7 +70,7 @@ def get_ids() -> Set[int]:
 
 def main():
     load_dotenv()
-    set_logging(intercept_nextcord_log=False, nextcord_loglevel=logging.INFO)
+    set_logging(intercept_nextcord_log=True, nextcord_loglevel=logging.WARNING)
 
     bot = JukeBot(
         command_prefix=get_prefix,
