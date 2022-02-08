085493a4055293ee9342e80f5e3f3842c1bc1e67
Dysta <xav4098@gmail.com>
2022-02-08 14:23:00 +0100

    [Enh] Remove useless commands

--- jukebot/cogs/utility.py
+++ jukebot/cogs/utility.py
@@ -57,18 +57,6 @@ class Utility(commands.Cog):
         await ctx.reply(embed=e)
 
     @commands.command(
-        brief="Check the bot latency",
-        help="Return the current latency of the bot.",
-    )
-    @commands.cooldown(1, 15.0, BucketType.user)
-    @commands.guild_only()
-    async def ping(self, ctx: Context):
-        e = embed.info_message(
-            ctx.author, content=f"Pong.. {self.bot.latency * 1000:.2f}ms"
-        )
-        await ctx.reply(embed=e)
-
-    @commands.command(
         aliases=["say"],
         brief="Repeat a message",
         help="Repeat message",
@@ -133,11 +121,6 @@ class Utility(commands.Cog):
             f"Thanks for support :heart:"
         )
         await ctx.send(content=msg)
-
-    @commands.command(hidden=True)
-    @commands.guild_only()
-    async def zizi(self, ctx: Context):
-        await ctx.reply("caca")
 
     @commands.command(
         aliases=["w"],
