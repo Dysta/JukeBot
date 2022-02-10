deb7a958af9d38b2152095b53204b96ecebc6838
Dysta <xav4098@gmail.com>
2022-02-10 12:19:35 +0100

    [Enh] Add more log info in logger handler

--- jukebot/listeners/logger_handler.py
+++ jukebot/listeners/logger_handler.py
@@ -11,8 +11,10 @@ class LoggerHandler(commands.Cog):
     async def on_command(self, ctx: Context):
         logger.opt(lazy=True).info(
             f"Server: '{ctx.guild.name}' (ID: {ctx.guild.id}) | "
+            f"Channel: '{ctx.channel.name}' (ID: {ctx.channel.id}) | "
             f"Invoker: '{ctx.author}' | "
             f"command: '{ctx.command.cog.qualified_name}:{ctx.command.name}' | "
+            f"parameters: '{ctx.args}' - '{ctx.kwargs}' | "
             f"raw message: '{ctx.message.content}'."
         )
 
@@ -20,8 +22,10 @@ class LoggerHandler(commands.Cog):
     async def on_command_invoked(self, ctx: Context, cmd: Command):
         logger.opt(lazy=True).info(
             f"Server: '{ctx.guild.name}' (ID: {ctx.guild.id}) | "
+            f"Channel: '{ctx.channel.name}' (ID: {ctx.channel.id}) | "
             f"Invoker: '{ctx.author}' | "
             f"command: '{ctx.command.cog.qualified_name if ctx.command else 'None'}:{ctx.command.name if ctx.command else 'None'} | "
+            f"parameters: '{ctx.args}' - '{ctx.kwargs}' | "
             f"invoked command: '{cmd.cog.qualified_name}:{cmd.name}' | "
             f"raw message: '{ctx.message.content}'."
         )
