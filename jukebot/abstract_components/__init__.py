ea8bb67a0aa931e0a4a5c3f11bca36f144723a4b
Dysta <xav4098@gmail.com>
2022-08-26 22:03:11 +0200

    feat: add abstract request component

--- jukebot/abstract_components/__init__.py
+++ jukebot/abstract_components/__init__.py
@@ -3,3 +3,4 @@ from .abstract_collection import AbstractCollection
 from .abstract_map import AbstractMap
 from .abstract_mongodb import AbstractMongoDB
 from .abstract_service import AbstractService
+from .abstract_request import AbstractRequest
---
+++ jukebot/abstract_components/abstract_request.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class AbstractRequest(ABC):
+    """Class that represent a request made by the user during a command."""
+
+    def __init__(self, query: str) -> None:
+        self._query: str = query
+        self._result: Any = None
+        self._success: bool = False
+
+    @abstractmethod
+    async def setup(self):
+        """Setup the Request. Called before the do method."""
+
+    @abstractmethod
+    async def execute(self):
+        """Execute the Request and handle the result type"""
+
+    @abstractmethod
+    async def terminate(self):
+        """Cleanup the Request. Called after the do method."""
+
+    async def __aenter__(self):
+        await self.setup()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.terminate()
+
+    @property
+    def query(self) -> str:
+        """Return the current query
+
+        Returns
+        -------
+        str
+            The current query
+        """
+        return self._query
+
+    @property
+    def result(self) -> Any:
+        """Return the current result
+
+        Returns
+        -------
+        Any
+            The current result
+        """
+        return self._result
+
+    @property
+    def success(self) -> bool:
+        """Return the current success
+
+        Returns
+        -------
+        bool
+            The current success
+        """
+        return self._success
