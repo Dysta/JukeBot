dc19aef25960549813f4d09baf0cd212fd2bc4be
Dysta <xav4098@gmail.com>
2022-08-31 18:03:24 +0200

    fix: tests for shazam request

--- tests/test_shazam_request.py
+++ tests/test_shazam_request.py
@@ -6,20 +6,17 @@ from jukebot.components.requests import ShazamRequest
 class TestShazamRequestComponent(unittest.IsolatedAsyncioTestCase):
     async def test_shazam_request_success(self):
         async with ShazamRequest(
-            "https://twitter.com/thibaultbrock/status/1499800398244335624"
+            "https://twitter.com/LaCienegaBlvdss/status/1501975048202166283"
         ) as req:
             await req.execute()
 
         self.assertTrue(req.success)
         result: dict = req.result
 
+        self.assertEqual(result.get("title"), "Enya - The Humming (Lyric Video)")
+        self.assertEqual(result.get("url"), "https://youtu.be/FOP_PPavoLA?autoplay=1")
         self.assertEqual(
-            result.get("title"),
-            "We Came As Romans - Black Hole  (Live from St. Andrew's Hall Detroit)",
-        )
-        self.assertEqual(result.get("url"), "https://youtu.be/4VPFMTSR8SU?autoplay=1")
-        self.assertEqual(
-            result.get("image_url"), "https://i.ytimg.com/vi/4VPFMTSR8SU/maxresdefault.jpg"
+            result.get("image_url"), "https://i.ytimg.com/vi/FOP_PPavoLA/maxresdefault.jpg"
         )
 
     async def test_shazam_request_failed(self):
