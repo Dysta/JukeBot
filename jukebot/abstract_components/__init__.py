e78c83878ac5b9fd52b8331ae0e7dacd6678ee95
Dysta <xav4098@gmail.com>
2023-05-18 15:49:45 +0200

    feat: remove mongodb connector

--- jukebot/abstract_components/__init__.py
+++ jukebot/abstract_components/__init__.py
@@ -1,6 +1,5 @@
 from .abstract_cache import AbstractCache
 from .abstract_collection import AbstractCollection
 from .abstract_map import AbstractMap
-from .abstract_mongodb import AbstractMongoDB
-from .abstract_service import AbstractService
 from .abstract_request import AbstractRequest
+from .abstract_service import AbstractService
